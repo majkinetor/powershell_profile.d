21826e33164f05c3f1fc0e6fed27c6fb00432403
Miodrag Milic <miodrag.milic@gmail.com>
2016-10-27 12:44:28 +0200

    git status and LEC

--- 10_prompt.ps1
+++ 10_prompt.ps1
@@ -8,7 +8,20 @@ function prompt
         if ($prp.IsInRole($adm)) { "(admin)" } 
     }
 
-    if (gcm RenameTab.cmd -ea 0) { RenameTab (Split-Path $pwd -Leaf) | out-null }
+    $lec = $LastExitCode
+    #if (gcm RenameTab.cmd -ea 0) { RenameTab (Split-Path $pwd -Leaf) | out-null }
+    if (gcm git.exe -ea 0) {
+        $gs = git status -sb 2>$null
+        if ($gs){
+            $git = @{}
+            $git.branch = $gs[0] -replace '## (.+?)\.\.\..+', '$1'
+            foreach ($file in ($gs | select -Skip 1).Trim()) {
+                $af = $file -split ' '
+                $git[ $af[0] ] += @(($af | select -skip 1) -join ' ')
+            }
+            $git.status = " [ M:{0} D:{1} A:{2} ?:{3} ]" -f $git.M.Length, $git.D.Length, $git.A.Length, $git['??'].Length
+        }
+    }
 
     $date   = Get-Date -Format "yy-MM-dd HH:mm:ss"
     $cmd_no = (history).length + 1
@@ -16,12 +29,17 @@ function prompt
     $color = if (admin) {"Yellow"} else { "Gray"}
     w ('-'*$Host.UI.RawUI.WindowSize.Width) $color
     w "$cmd_no"   Yellow
+    if ($lec -ne 0) { w " LEC $lec" Red }
+    if ($git) { w $git.status Green }
     w "  "        Gray
     w $date       Red
     w "  "        Gray
     w "$pwd"      Green
     w " $(admin)" Yellow
     write-host
+    if ($git) { w $git.branch Red }
     w ">"
+
+    $LastExitCode = $lec
     return " "
  }
