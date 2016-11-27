61da3f8296467e28cdce7197aa7b56877ff5a80c
Miodrag Milic <miodrag.milic@gmail.com>
2016-11-27 09:55:55 +0100

    tgit url

--- 99_general.ps1
+++ 99_general.ps1
@@ -9,4 +9,5 @@ $PSSessionOption = New-PSSessionOption -ProxyAccessType NoProxyServer
 #delete some aliases
 'wget', 'curl' | % { rm  Alias:\$_ -ea 0 }
 
+#https://tortoisegit.org/docs/tortoisegit/tgit-automation.html
 function tgit() { & "C:\Program Files\TortoiseGit\bin\TortoiseGitProc.exe" "/command:$args" /path:. }
