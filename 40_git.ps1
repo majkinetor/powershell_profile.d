4bc0277fdfe3a3b3719b64857c47d14e0507ad0b
Miodrag Milic <miodrag.milic@gmail.com>
2016-12-05 13:05:22 +0100

    typo

--- 40_git.ps1
+++ 40_git.ps1
@@ -6,6 +6,6 @@ function tgit( $Command, $Path = '.' ) {
 
 # Ask for password in GUI (easier to see and automate)
 #https://marcus.handte.org/2011/09/07/enabling-interactive-prompts-in-cygwins-git-port/
-sshaskpass_path = 'C:\Program Files\TortoiseGit\bin\sshaskpass.exe'
+$sshaskpass_path = 'C:\Program Files\TortoiseGit\bin\sshaskpass.exe'
 if (Test-Path $sshaskpass_path) { $ENV:GIT_ASKPASS  = $sshaskpass_path }
 rm Variable:/sshaskpass_path
