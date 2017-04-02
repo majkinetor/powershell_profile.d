fbf170a06658c156348795442efbec2daa7de866
Miodrag Milic <miodrag.milic@gmail.com>
2017-04-02 15:53:20 +0200

    improved tgit

--- 40_git.ps1
+++ 40_git.ps1
@@ -1,18 +1,16 @@
 if (!(gcm git -ea 0)) { Write-Warning "Git profile: git is not found on path" }
 
-# Drive TortoiseGit via command line
-#https://tortoisegit.org/docs/tortoisegit/tgit-automation.html
-function tgit( $Command='commit', $Path = '.' ) {
-    if ($Path -eq '/') { $Path = git rev-parse --show-toplevel }
-    & "$Env:ProgramFiles\TortoiseGit\bin\TortoiseGitProc.exe" "/command:$Command" "/path:$Path"
+# Set TortoiseGit helpers
+$tortoise_git_path = "$Env:ProgramFiles\TortoiseGit"
+if (Test-Path $tortoise_git_path) {
+    . $PSScriptRoot\scripts\tgit.ps1
+
+    # Ask for password in GUI (easier to see and automate)
+    #https://marcus.handte.org/2011/09/07/enabling-interactive-prompts-in-cygwins-git-port/
+    $ENV:GIT_ASKPASS  = "$tortoise_git_path\bin\sshaskpass.exe"
 }
+rm Variable:/tortoise_git_path
 
-# Ask for password in GUI (easier to see and automate)
-#https://marcus.handte.org/2011/09/07/enabling-interactive-prompts-in-cygwins-git-port/
-$sshaskpass_path = 'C:\Program Files\TortoiseGit\bin\sshaskpass.exe'
-if (Test-Path $sshaskpass_path) { $ENV:GIT_ASKPASS  = $sshaskpass_path } else { Write-Warning 'Git profile: sshaskpass not found' }
-rm Variable:/sshaskpass_path
-
-#Set git editor
+# Set git editor
 if (gcm gvim -ea 0) { $Env:GIT_EDITOR = 'gvim' } else { Write-Warning 'Git profile: gvim not found' }
 
