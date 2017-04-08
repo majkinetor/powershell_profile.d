9558031391ae847654de53951e782d704540cd2e
Miodrag Milić <miodrag.milic@gmail.com>
2017-04-08 17:25:48 +0200

    published on gallery

--- scripts/tgit.ps1
+++ scripts/tgit.ps1
@@ -1,31 +1,36 @@
-# Last Change: 02-Apr-2017.
-# Author: M. Milic <miodrag.milic@gmail.com>
-
-<#
-.SYNOPSIS
-    Drive TortoiseGit via command line
-
-.DESCRIPTION
-    A simple script to be able to run TortoiseGit options via command line instead invoking it using GUI.
-
-.EXAMPLE
-    tgit commit /
-
-    Open commit dialog from the root of the repository
-
-.LINK
-    https://tortoisegit.org/docs/tortoisegit/tgit-automation.html
+<#PSScriptInfo 
+.VERSION 0.2 
+.GUID 88f076e7-0c1f-48b3-a122-29cea3c34728 
+.AUTHOR miodrag.milic@gmail.com 
+.COPYRIGHT Miodrag Milic 
+.TAGS git tortoisegit 
+.LICENSEURI https://opensource.org/licenses/MIT 
+.PROJECTURI https://github.com/majkinetor/powershell_profile.d/blob/master/scripts/tgit.ps1 
 #>
-function tgit()  {
-    param(
-        # TortoiseGit command
-        [ValidateSet('fetch', 'firststart', 'log', 'clone', 'commit', 'add', 'revert', 'cleanup', 'resolve', 'repocreate', 'switch', 'export', 'merge', 'settings', 'remove', 'rename', 'diff', 'showcompare', 'conflicteditor', 'help', 'repostatus', 'repobrowser', 'ignore', 'blame', 'cat', 'pull', 'push', 'rebase', 'stashsave', 'stashapply', 'stashpop', 'subadd', 'subupdate', 'subsync', 'reflog', 'refbrowse', 'updatecheck', 'revisiongraph', 'daemon', 'tag')]
-        [string] $Command='commit',
 
-        # Many commands require PATH argument. Special value '/' means root of the repository.
-        [string] $Path = '.'
-    )
+<# 
+.SYNOPSIS 
+    Drive TortoiseGit via command line 
+ 
+.DESCRIPTION 
+    TortoiseGit simple CLI interface 
+ 
+.EXAMPLE 
+    tgit commit / 
+ 
+    Open commit dialog from the root of the repository 
+ 
+.LINK 
+    https://tortoisegit.org/docs/tortoisegit/tgit-automation.html 
+#> 
+param(
+    # TortoiseGit command
+    [ValidateSet('fetch', 'firststart', 'log', 'clone', 'commit', 'add', 'revert', 'cleanup', 'resolve', 'repocreate', 'switch', 'export', 'merge', 'settings', 'remove', 'rename', 'diff', 'showcompare', 'conflicteditor', 'help', 'repostatus', 'repobrowser', 'ignore', 'blame', 'cat', 'pull', 'push', 'rebase', 'stashsave', 'stashapply', 'stashpop', 'subadd', 'subupdate', 'subsync', 'reflog', 'refbrowse', 'updatecheck', 'revisiongraph', 'daemon', 'tag')]
+    [string] $Command='commit',
 
-    if ($Path -eq '/') { $Path = git rev-parse --show-toplevel }
-    & "$Env:ProgramFiles\TortoiseGit\bin\TortoiseGitProc.exe" "/command:$Command" "/path:$Path"
-}
+    # Many commands require PATH argument. Special value '/' means root of the repository.
+    [string] $Path = '.'
+)
+
+if ($Path -eq '/') { $Path = git rev-parse --show-toplevel }
+& "$Env:ProgramFiles\TortoiseGit\bin\TortoiseGitProc.exe" "/command:$Command" "/path:$Path"
