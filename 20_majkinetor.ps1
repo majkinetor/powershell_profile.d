29e0f62899ce36d5a4812d95e18994d90c2f1d78
Miodrag Milic <miodrag.milic@gmail.com>
2017-06-05 20:47:59 +0200

    majkinetor module fixes

--- 20_majkinetor.ps1
+++ 20_majkinetor.ps1
@@ -1,13 +1,13 @@
 # profile_modules_path could be set in $PROFILE
-if (!$profile_modules_path) { $profile_modules_path = Resolve-Path "$PSScriptRoot\..\posh" }
+if (!$profile_modules_path) { $profile_modules_path = Resolve-Path "$PSScriptRoot\..\posh" -ea 0 }
 
-if (Test-Path $profile_modules_path) {
+if ($profile_modules_path -and (Test-Path $profile_modules_path)) {
     $Env:PSModulePath += ";$profile_modules_path"
     Write-Warning "Importing majkinetor's posh modules"
     import-module mm_sugar, mm_network, mm_admin        #auto-load not working ?
     Set-AppKeyAliases *> $null
 } else {
-    Write-Warning "majkinetor's modules not found at '$profile_modules_path'`ngit clone https://github.com/majkinetor/posh $profile_modules_path"
+    Write-Warning "majkinetor's modules not found at $(Resolve-Path $PSScriptRoot\..)\posh`n         git clone https://github.com/majkinetor/posh $profile_modules_path"
 }
 
 rm Variable:/profile_modules_path
