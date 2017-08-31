2d5f43fea8ce1a0542516adda2ff4065c8eb8371
Miodrag Milic <miodrag.milic@gmail.com>
2017-08-31 07:13:28 +0200

    added menu complete

--- 99_general.ps1
+++ 99_general.ps1
@@ -1,18 +1,21 @@
+# Show menu on {Tab} ASAP
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
 # Attach spawned intances to conemu
 if (Test-Path $Env:ProgramFiles\ConEmu) { & $Env:ProgramFiles\ConEmu\ConEmu\ConEmuC.exe /AUTOATTACH }
 
 # Remove PSCX less helper, I already use my own less
-if (gmo -List pscx) { ($Pscx:Preferences).PageHelpUsingLess = $false }
+# if (gmo -List pscx) { ($Pscx:Preferences).PageHelpUsingLess = $false }
 
 # Do not use any proxy server for posh remoting
 $PSSessionOption = New-PSSessionOption -ProxyAccessType NoProxyServer
 
-# Prevent garbage in logs
+# Use UTF8
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
-# Delete some aliases that I actually use original tools
+# Delete some aliases for which I use original tools
 'wget', 'curl' | % { rm  Alias:\$_ -ea 0 }
 
-# Force AU update
+# Force AU update shortcut
 function fup() { $global:au_force = $true; ./update.ps1}
 
