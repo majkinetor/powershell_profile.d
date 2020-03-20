d5afc4f66465173ddccaee99c2932bf81d144f73
Miodrag Milic <miodrag.milic@gmail.com>
2020-03-20 12:37:09 +0100

    s

--- 20_majkinetor.ps1
+++ 20_majkinetor.ps1
@@ -5,7 +5,7 @@ if ($profile_modules_path -and (Test-Path $profile_modules_path)) {
     $Env:PSModulePath += ";$profile_modules_path"
     Write-Warning "Importing majkinetor's posh modules"
     #import-module mm_sugar, mm_network, mm_admin        #auto-load not working ?
-    Set-AppKeyAliases *> $null
+    #Set-AppKeyAliases *> $null
 } else {
     Write-Warning "majkinetor's modules not found at $(Resolve-Path $PSScriptRoot\..)\posh`n         git clone https://github.com/majkinetor/posh $profile_modules_path"
 }
