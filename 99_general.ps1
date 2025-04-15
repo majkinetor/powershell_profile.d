f88c575507a320cb52ebef1e24c76f1054a1ff98
Miodrag Milic <mmilic@knowit.rs>
2025-04-15 12:03:16 +0200

    ib argumentcompleter; removed choco

--- 99_general.ps1
+++ 99_general.ps1
@@ -2,7 +2,7 @@
 #Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
 # Attach spawned intances to conemu
-if (Test-Path $Env:ProgramFiles\ConEmu\ConEmuC.exe) { & $Env:ProgramFiles\ConEmu\ConEmu\ConEmuC.exe /AUTOATTACH }
+#if (Test-Path $Env:ProgramFiles\ConEmu\ConEmuC.exe) { & $Env:ProgramFiles\ConEmu\ConEmu\ConEmuC.exe /AUTOATTACH }
 
 # Remove PSCX less helper, I already use my own less
 # if (gmo -List pscx) { ($Pscx:Preferences).PageHelpUsingLess = $false }
@@ -16,11 +16,10 @@ $PSSessionOption = New-PSSessionOption -ProxyAccessType NoProxyServer
 # Delete some aliases for which I use original tools
 'wget', 'curl' | % { rm  Alias:\$_ -ea 0 }
 
-# Force AU update shortcut
-function fup() { $global:au_force = $true; ./update.ps1}
-
 sal pester Invoke-Pester
-sal ib     Invoke-Build
-if (Test-Path $env:ChocolateyInstall\lib\invoke-build) { & "$env:ChocolateyInstall\lib\invoke-build\tools\Invoke-Build.ArgumentCompleters.ps1" }
+
+sal ib Invoke-Build
+& $PSScriptRoot\scripts\Invoke-Build.ArgumentCompleters.ps1
+
 
 #[System.Net.ServicePointManager]::SecurityProtocol = 3072 -bor 768 -bor [System.Net.SecurityProtocolType]::Tls -bor [System.Net.SecurityProtocolType]::Ssl3
